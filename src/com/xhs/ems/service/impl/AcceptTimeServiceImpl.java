79d9003a9c2b1410f048cbe39958f92e4d990012
cuixingwei <1429226085@qq.com>
2015-07-30 21:58:52 +0800

    调整一个service的文件到impl目录下

--- src/com/xhs/ems/service/impl/AcceptTimeServiceImpl.java
+++ src/com/xhs/ems/service/impl/AcceptTimeServiceImpl.java
@@ -1,4 +1,4 @@
-package com.xhs.ems.service;
+package com.xhs.ems.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.xhs.ems.bean.Grid;
 import com.xhs.ems.bean.Parameter;
 import com.xhs.ems.dao.AcceptTimeDAO;
+import com.xhs.ems.service.AcceptTimeService;
 
 @Service
 public class AcceptTimeServiceImpl implements AcceptTimeService {
